e74b4b4c8f866bd0468d12637b417c2b5fd6365a
Nilesh Pant <i.nilesh.pant@redcarpetup.com>
2020-08-21 02:34:05 +0530

    added sidebar of youtube

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import './Sidebar.css'
+import SidebarRow from "./SidebarRow"
+import WhatshotIcon from "@material-ui/icons/Whatshot"
+import HomeIcon from "@material-ui/icons/Home"
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions"
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary"
+import HistoryIcon from "@material-ui/icons/History"
+import OndemandVideoIcon from "@material-ui/icons/OndemandVideo"
+import WatchLaterIcon from "@material-ui/icons/WatchLater"
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined"
+import ExpandMoreOutLined from "@material-ui/icons/ExpandMoreOutlined"
+
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1> Videos</h1>
+            <SidebarRow selected Icon={HomeIcon} title="Home" />
+            <SidebarRow Icon={WhatshotIcon} title="Trending" />
+            <SidebarRow Icon={SubscriptionsIcon} title="Sunscription" />
+            <hr />
+
+            <SidebarRow Icon={VideoLibraryIcon} title="Library" />
+            <SidebarRow Icon={HistoryIcon} title="History" />
+            <SidebarRow Icon={OndemandVideoIcon} title="Subscription" />
+            <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
+            <SidebarRow Icon={ThumbUpAltOutlinedIcon} title="Liked Videos" />
+            <SidebarRow Icon={ExpandMoreOutLined} title="Show more" />
+
         </div>
     )
 }

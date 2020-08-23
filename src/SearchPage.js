4738ad91276712aa6812d01dd1c69257827e92cc
Nilesh Pant <nileshpant6969@gmail.com>
2020-08-24 00:36:00 +0530

    search page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./SearchPage.css"
 import ChannelRow from "./ChannelRow.js"
 import TuneOutlinedIcon from "@material-ui/icons/TuneOutlined"
+import VideoRow from "./VideoRow.js"
 
 function SearchPage() {
     return (
@@ -19,6 +20,71 @@ function SearchPage() {
                 subs="999k"
                 noOfVideos={124}
                 description="you can find awesome programming lessons here> Also, except programming tips and tricks that will take your coding to the mext level"
+            />
+
+            <hr />
+            <VideoRow
+                description="A little bit about me, I’m a 21yo developer from Tunisia (North Africa) with no special superpowers. The only superpower I have is my passion for coding and helping others."
+                image="https://images.indianexpress.com/2019/11/ms-dhoni-1200.jpg"
+                views="2.2M Views"
+                subs="567k"
+                timestamp="1 days ago"
+                title="Legecy of MS Dhoni"
+                channel="ICC"
+            />
+            <VideoRow
+                description="A little bit about me, I’m a 21yo developer from Tunisia (North Africa) with no special superpowers. The only superpower I have is my passion for coding and helping others."
+                image="https://images.indianexpress.com/2019/11/ms-dhoni-1200.jpg"
+                views="2.2M Views"
+                subs="567k"
+                timestamp="1 days ago"
+                title="Legecy of MS Dhoni"
+                channel="ICC"
+            />
+            <VideoRow
+                description="A little bit about me, I’m a 21yo developer from Tunisia (North Africa) with no special superpowers. The only superpower I have is my passion for coding and helping others."
+                image="https://images.indianexpress.com/2019/11/ms-dhoni-1200.jpg"
+                views="2.2M Views"
+                subs="567k"
+                timestamp="1 days ago"
+                title="Legecy of MS Dhoni"
+                channel="ICC"
+            />
+            <VideoRow
+                description="A little bit about me, I’m a 21yo developer from Tunisia (North Africa) with no special superpowers. The only superpower I have is my passion for coding and helping others."
+                image="https://images.indianexpress.com/2019/11/ms-dhoni-1200.jpg"
+                views="2.2M Views"
+                subs="567k"
+                timestamp="1 days ago"
+                title="Legecy of MS Dhoni"
+                channel="ICC"
+            />
+            <VideoRow
+                description="A little bit about me, I’m a 21yo developer from Tunisia (North Africa) with no special superpowers. The only superpower I have is my passion for coding and helping others."
+                image="https://images.indianexpress.com/2019/11/ms-dhoni-1200.jpg"
+                views="2.2M Views"
+                subs="567k"
+                timestamp="1 days ago"
+                title="Legecy of MS Dhoni"
+                channel="ICC"
+            />
+            <VideoRow
+                description="A little bit about me, I’m a 21yo developer from Tunisia (North Africa) with no special superpowers. The only superpower I have is my passion for coding and helping others."
+                image="https://images.indianexpress.com/2019/11/ms-dhoni-1200.jpg"
+                views="2.2M Views"
+                subs="567k"
+                timestamp="1 days ago"
+                title="Legecy of MS Dhoni"
+                channel="ICC"
+            />
+            <VideoRow
+                description="A little bit about me, I’m a 21yo developer from Tunisia (North Africa) with no special superpowers. The only superpower I have is my passion for coding and helping others."
+                image="https://images.indianexpress.com/2019/11/ms-dhoni-1200.jpg"
+                views="2.2M Views"
+                subs="567k"
+                timestamp="1 days ago"
+                title="Legecy of MS Dhoni"
+                channel="ICC"
             />
         </div>
     )

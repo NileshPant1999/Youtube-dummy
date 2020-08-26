4bb9add79094667bfcaa8f272323ac914a529a19
Nilesh Pant <nileshpant6969@gmail.com>
2020-08-27 02:23:52 +0530

    Updates

---
+++ precache-manifest.debc3bc95874a25155727f0f8aef82a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a219e94c53a08f87a0431b6bc3c30811",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1da63520a5d59010eb16",
+    "url": "/static/css/main.12669689.chunk.css"
+  },
+  {
+    "revision": "d3ee1a3b9ecf5815b2fc",
+    "url": "/static/js/2.c16d4f65.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c16d4f65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1da63520a5d59010eb16",
+    "url": "/static/js/main.a87017bc.chunk.js"
+  },
+  {
+    "revision": "35d41d49a0a9e200cdd9",
+    "url": "/static/js/runtime-main.4eaf399a.js"
+  }
+]);
